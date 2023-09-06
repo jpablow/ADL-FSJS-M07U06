c581e25163180a860608a230d7d8b073cb77a3dd
Juan Pablo Villanueva <juanpablow@gmail.com>
2023-09-05 23:36:54 -0300

    agrega gitignore

--- consultas.js
+++ consultas.js
@@ -27,6 +27,15 @@ const registrarUsuario = async (usuario) => {
   }
 };
 
-const verificarCredenciales = async (email, password) => {};
+const verificarCredenciales = async (email, password) => {
+  const consulta = 'SELECT * FROM usuarios WHERE email = $1 AND password = $2';
+  const values = [email, password];
+  const { rowCount } = await pool.query(consulta, values);
+  if (!rowCount)
+    throw {
+      code: 404,
+      message: 'No se encontró ningún usuario con estas credenciales',
+    };
+};
 
 module.exports = { registrarUsuario, verificarCredenciales };
